017d71663c6675ef0c014280a439e1e57b240bd3
Zakk Roberts <z-roberts@users.noreply.github.com>
2021-12-06 21:22:51 +0000

    Adding new files for Lab 6

---
+++ py38.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:deadsnakes/ppa -y
+sudo apt-get update
+sudo apt-get install python3.8 python3.8-venv python3.8-dev python3-pip build-essential -y
